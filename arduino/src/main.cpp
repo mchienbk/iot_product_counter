1235b42c696a27364a231e40f546bc2e998d8300
Tran Minh Chien <minhchien2031995@gmail.com>
2020-11-11 21:17:32 +0700

    update program 11.11.20

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,29 +1,155 @@
-#include "header.h"
+#include <Arduino.h>
 
 
+#define P_OUT_01  2
+#define P_OUT_02  3
+#define P_OUT_03  4
+#define P_OUT_04  5
+#define P_OUT_05  6
+#define P_OUT_06  7
+#define P_OUT_07  8
+#define P_OUT_08  9
+#define P_OUT_09  12
+#define P_OUT_10  13      // Led built-in
+
+#define P_PWM_01  10
+#define P_PWM_02  11
+
+#define INT_CN0 0
+
+#define PRO_ID_01 5
+#define PRO_ID_02 4
+#define PRO_ID_03 0
+
+volatile int btn_id = 0;
+
+char incomingByte;
+// char process_sst = false;
+// int check_rcv = 0;
+int i;
+// void get_btn_id(void);
+void process_1(void);
+void process_2(void);
+void process_3(void);
+void senddata(char data);
 
 void setup()
 {
-  pinMode(P_IN_01, INPUT);
-  pinMode(P_IN_02, INPUT);
-  pinMode(P_IN_03, INPUT);
-
   pinMode(P_OUT_01, OUTPUT);
   pinMode(P_OUT_02, OUTPUT);
   pinMode(P_OUT_03, OUTPUT);
   pinMode(P_OUT_04, OUTPUT);
   pinMode(P_OUT_05, OUTPUT);
   pinMode(P_OUT_06, OUTPUT);
+  pinMode(P_OUT_07, OUTPUT);
+  pinMode(P_OUT_08, OUTPUT);
+  pinMode(P_OUT_09, OUTPUT);
+  pinMode(P_OUT_10, OUTPUT);
+  pinMode(P_PWM_01, OUTPUT);
+  pinMode(P_PWM_02, OUTPUT);
+  digitalWrite(P_OUT_01, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_02, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_03, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_04, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_05, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_06, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_07, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_08, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_09, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_10, HIGH);     // Output HIGH
 
-  pinMode(PWM_01, OUTPUT);
-  pinMode(PWM_02, OUTPUT);
-
-  attachInterrupt(INT_CN0, get_btn_id, LOW); // pin 2
-
+  for(i = 0; i < 50; i++){
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(50);
+  }
   Serial.begin(9600);
-  // Serial.println("You are welcome");
-
+  delay(200);
 }
+void loop()
+{
+  if (Serial.available() > 0) {       // Check request signal
+    incomingByte = Serial.read();
+    // Serial.print("Received: ");
+    // Serial.println(incomingByte);
+
+    switch (incomingByte)
+    {
+    case PRO_ID_01:
+      process_1();
+      senddata(PRO_ID_01);
+      break;
+
+    case PRO_ID_02:
+      process_2();
+      senddata(PRO_ID_02);
+      break;
+
+    case PRO_ID_03:
+      process_3();
+      senddata(PRO_ID_03);
+      break;
+
+    default:
+      break;
+    }
+  }
+}
+
+
+
+
+// Code for prosess 01
+void process_1(void){
+  digitalWrite(P_OUT_01, LOW);      // Output LOW
+  delay(5000);
+  digitalWrite(P_OUT_01, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_02, LOW);      // Output LOW
+  delay(5000);
+  digitalWrite(P_OUT_02, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_03, LOW);      // Output LOW
+  delay(5000);
+  digitalWrite(P_OUT_03, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_04, LOW);      // Output LOW
+  delay(5000); 
+  digitalWrite(P_OUT_04, HIGH);     // Output HIGH
+  delay(5000);
+}
+
+// Code for prosess 02
+void process_2(void){
+  digitalWrite(P_OUT_05, LOW);      // Output LOW
+  delay(5000);
+  digitalWrite(P_OUT_05, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_06, LOW);      // Output LOW
+  delay(5000);
+  digitalWrite(P_OUT_06, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_07, LOW);      // Output LOW
+  delay(5000);
+  digitalWrite(P_OUT_07, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_08, LOW);      // Output LOW
+  delay(5000); 
+  digitalWrite(P_OUT_08, HIGH);     // Output HIGH
+  delay(5000); 
+}
+
+// Code for prosess 03
+void process_3(void){
+  digitalWrite(P_OUT_09, LOW);      // Output LOW
+  delay(5000);
+  digitalWrite(P_OUT_09, HIGH);     // Output HIGH
+  digitalWrite(P_OUT_10, LOW);      // Output LOW
+  delay(5000);
+}
+
+void senddata(char data){
+  Serial.print(data);
+}
+
+
+#if 0
+
 void loop()
 {
   // switch(btn_id){
@@ -51,16 +177,10 @@ void loop()
   // }
   // delay(100);
 
-
-  senddata(49);
-  delay(2000);
-
-  senddata(50);
-  delay(2000);
-
-  senddata(51);
-  delay(2000);
 }
+
+
+
 
 // Get button which pressed
 void get_btn_id()
@@ -79,46 +199,7 @@ void get_btn_id()
     btn_id = 0;
   }
 }
+#endif
 
-void process_1(void){
-  digitalWrite(P_OUT_01, LOW);
-  digitalWrite(P_OUT_02, HIGH);
-  digitalWrite(P_OUT_03, HIGH);
 
-  analogWrite(PWM_01,255);
-  btn_id = 0;
-}
 
-void process_2(void){
-  digitalWrite(P_OUT_01, HIGH);
-  digitalWrite(P_OUT_02, LOW);
-  digitalWrite(P_OUT_03, HIGH);
-  
-  analogWrite(PWM_02,255);
-  btn_id = 0;
-}
-
-void process_3(void){
-  digitalWrite(P_OUT_01, HIGH);
-  digitalWrite(P_OUT_02, HIGH);
-  digitalWrite(P_OUT_03, LOW);
-  analogWrite(PWM_01,0);
-  analogWrite(PWM_02,0);
-  btn_id = 0;
-}
-
-void senddata(char data){
-  check_rcv = 0;
-  Serial.print(data);
-  // while(true){
-  //   if (Serial.available() > 0) {
-  //     check_rcv++;
-  //     if(Serial.read()==1)
-  //       break;
-  //     if(check_rcv>1000){
-  //       Serial.println("Error");
-  //       break;
-  //     }
-  //   }
-  // }
-}
--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -1,79 +1,239 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include "FirebaseESP8266.h"
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
-// Set these to run example.
+#define FIREBASE_HOST "wash-station.firebaseio.com"
+#define FIREBASE_AUTH "HtkscMdQrWHWlNLQJb7EjKJuYjp0DW4sL1oieVME"
+#define WIFI_SSID "ONOWASH_2.4G"
+#define WIFI_PASSWORD "023456789"
 
-#define FIREBASE_HOST   "wash-station.firebaseio.com"
-#define FIREBASE_AUTH   "HtkscMdQrWHWlNLQJb7EjKJuYjp0DW4sL1oieVME"
-#define WIFI_SSID       "Wireless-HN"
-#define WIFI_PASSWORD   "vohongnhi"
+#define BTN_01 5
+#define BTN_02 4
+#define BTN_03 0
 
+// Firebase database
 FirebaseData firebaseData;
 FirebaseJson jsonStr;
+
+// Define NTP Client to get time
+const long utcOffsetInSeconds = 7 * 3600;
+char daysOfTheWeek[7][5] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+void Send_to_uno(char data);
 
 int ledStatus = 0; // Led status received
 
 int data1, data2, data3;
-int i = 0;
+int toDay, thisDay;
+// int i = 0;
 char incomingByte;
+String data_folder_1;
+String data_folder_2;
+String data_folder_3;
+int i;
 
 void setup()
 {
+    pinMode(BTN_01, INPUT); // Button 01 input
+    pinMode(BTN_02, INPUT); // Button 02 input
+    pinMode(BTN_03, INPUT); // Button 03 input
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+    for(i = 0; i < 50; i++){
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(50);
+    }
+    Serial.begin(9600);
 
-  Serial.begin(9600);
- 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    // Serial.print("Connecting");
 
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(500);
-  }
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        // Serial.print(".");
+        delay(500);
+    }
 
+    // // Print local IP address
+    // Serial.println();
+    // Serial.print("Connected to ");
+    // Serial.println(WIFI_SSID);
+    // Serial.print("IP Address is : ");
+    // Serial.println(WiFi.localIP());
 
-  Serial.println();
-  Serial.print("Connected to ");
-  Serial.println(WIFI_SSID);
-  Serial.print("IP Address is : ");
+    // Start timeClient
+    timeClient.begin();
 
-  // Print local IP address
-  Serial.println(WiFi.localIP());
+    // Start server
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    delay(1000);
 
-  
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  // Test set string value
-  // Test set string value
-  if (Firebase.setString(firebaseData, "message", "hello world") != 1){
-    Serial.print("setting /message failed:");
-    Serial.println(firebaseData.errorReason());
-    return;
-  }
-  delay(1000);
-  // Firebase Set Led status
-  Firebase.setBool(firebaseData, "LED_STATUS", 0);
-  delay(1000);
+    // Test server
+    // if (Firebase.setString(firebaseData, "message", "hello world") != 1){
+    //   Serial.print("setting /message failed:");
+    //   Serial.println(firebaseData.errorReason());
+    //   return;
+    // }
 
+    // Update firebase folder
+    timeClient.update();
+    toDay = timeClient.getDay();
+    data_folder_1 = "/userdata/" + (String)(daysOfTheWeek[toDay]) + "/port1";
+    data_folder_2 = "/userdata/" + (String)(daysOfTheWeek[toDay]) + "/port2";
+    data_folder_3 = "/userdata/" + (String)(daysOfTheWeek[toDay]) + "/port3";
+    thisDay = toDay;
+
+    Firebase.setInt(firebaseData, data_folder_1, 0);
+    Firebase.setInt(firebaseData, data_folder_2, 0);
+    Firebase.setInt(firebaseData, data_folder_3, 0);
 }
 
 void loop()
 {
+    // timeClient.update();
+    // toDay = timeClient.getDay();
 
+    // if(toDay != thisDay){
+    //     data_folder_1 = "/userdata/" + (String)(daysOfTheWeek[toDay]) + "/port1";
+    //     data_folder_2 = "/userdata/" + (String)(daysOfTheWeek[toDay]) + "/port2";
+    //     data_folder_3 = "/userdata/" + (String)(daysOfTheWeek[toDay]) + "/port3";
+    //     Firebase.setInt(firebaseData, data_folder_1, 0);
+    //     Firebase.setInt(firebaseData, data_folder_2, 0);
+    //     Firebase.setInt(firebaseData, data_folder_3, 0);
+    //     thisDay = toDay;    
+    // }
+
+    // Check button 01 was pressed
+    if (digitalRead(BTN_01) == LOW)
+    {
+        while (digitalRead(BTN_01) == LOW)
+        { // Wait for unpressed
+            delay(100);
+        }
+
+        digitalWrite(LED_BUILTIN, LOW); // Working-LED ON
+
+        Send_to_uno(BTN_01); // Send start signal 01
+        while (true)
+        {
+            if (Serial.available() > 0)
+            { // Check finish signal 01
+                incomingByte = Serial.read();
+                // Serial.print("Received: ");
+                // Serial.println(incomingByte);
+                if (incomingByte == BTN_01)
+                {
+                    // Serial.println("Done");
+                    digitalWrite(LED_BUILTIN, HIGH); // Working-LED OFF
+
+                    break;
+                }
+            }
+            delay(10);
+        }
+        // Update data 01
+        if (Firebase.getInt(firebaseData, data_folder_1))
+            data1 = firebaseData.intData() + 1;       
+        Firebase.setInt(firebaseData, data_folder_1, data1);
+    }
+
+    // Check button 02 was pressed
+    if (digitalRead(BTN_02) == LOW)
+    {
+        while (digitalRead(BTN_02) == LOW)
+        { // Wait for unpressed
+            delay(100);
+        }
+
+        digitalWrite(LED_BUILTIN, LOW); // Working-LED ON
+
+        Send_to_uno(BTN_02); // Send start signal 02
+        while (true)
+        {
+            if (Serial.available() > 0)
+            { // Check finish signal 02
+                incomingByte = Serial.read();
+                // Serial.print("Received: ");
+                // Serial.println(incomingByte);
+                if (incomingByte == BTN_02)
+                {
+                    // Serial.println("Done");
+                    digitalWrite(LED_BUILTIN, HIGH); // Working-LED OFF
+
+                    break;
+                }
+            }
+            delay(10);
+        }
+        // Update data 02
+        if (Firebase.getInt(firebaseData, data_folder_2))
+            data2 = firebaseData.intData() + 1;       
+        Firebase.setInt(firebaseData, data_folder_2, data2);
+    }
+
+    // Check button 03 was pressed
+    if (digitalRead(BTN_03) == LOW)
+    {
+        while (digitalRead(BTN_03) == LOW)
+        { // Wait for unpressed
+            delay(100);
+        }
+
+        digitalWrite(LED_BUILTIN, LOW); // Working-LED ON
+
+        Send_to_uno(BTN_03); // Send start signal 01
+        while (true)
+        {
+            if (Serial.available() > 0)
+            { // Check finish signal 01
+                incomingByte = Serial.read();
+                // Serial.print("Received: ");
+                // Serial.println(incomingByte);
+                if (incomingByte == BTN_03)
+                {
+                    // Serial.println("Done");
+                    digitalWrite(LED_BUILTIN, HIGH); // Working-LED OFF
+
+                    break;
+                }
+            }
+            delay(10);
+        }
+        // Update data 03
+        if (Firebase.getInt(firebaseData, data_folder_3))
+            data3 = firebaseData.intData() + 1;       
+        Firebase.setInt(firebaseData, data_folder_3, data3);
+    }
+}
+
+void Send_to_uno(char data)
+{
+    Serial.print(data);
+}
+
+#if 0
+void loop()
+{
   // Get data from firebase
-  // if (Firebase.getBool(firebaseData, "LED_STATUS"))
-  //   ledStatus = firebaseData.boolData();
-  // if (ledStatus == 0){
-  //   Serial.println("Led Turned ON");
-  //   digitalWrite(LED_BUILTIN, LOW);
-  // }
-  // else{
-  //   Serial.println("Led Turned OFF");
-  //   digitalWrite(LED_BUILTIN, HIGH);
-  // }
-
+  if (Firebase.getBool(firebaseData, "LED_STATUS"))
+    ledStatus = firebaseData.boolData();
+  if (ledStatus == 0){
+    Serial.println("Led Turned ON");
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else{
+    Serial.println("Led Turned OFF");
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  
+  
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
@@ -110,3 +270,9 @@ void loop()
     Serial.println(data3);
   }
 }
+
+// Update data 01
+// if (Firebase.getInt(firebaseData, "/userdata/port1"))
+//     data1 = firebaseData.intData() + 1;
+// Firebase.setInt(firebaseData, "/userdata/port1", data1);
+#endif
